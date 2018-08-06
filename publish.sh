7687c936ebb4ac6e8f81dc555ea52446e8bec1c2
Nico Linder <nico@myboshi.net>
2018-08-06 11:09:31 +0200

    fix codefactor issues

--- publish.sh
+++ publish.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 echo "Start publishing by building:"
 dotnet restore
-cd Andromeda.ConsoleApp
-  dotnet fake build --target Publish
-  cd ..
+(
+  cd Andromeda.ConsoleApp || exit
+  dotnet fake build --target publish
+)
 
 rm -fr "deploy"
 mkdir -p "deploy"
 
-cd "deploy"
+(
+  cd "deploy" || exit
   # Move files to publish folder
   mkdir "publish"
   mv "../Andromeda.ConsoleApp/bin/Release/netcoreapp2.1/publish" "publish/bin"
   cp "../build/start.cmd" "publish"
   cp "../build/start.sh" "publish"
-
-  cd "publish"
+  (
+    cd "publish" || exit
     echo "Put files into .zip .."
     find . -print | zip -q "../publish" -@
 
     echo "Put files into .tar .."
-    tar -czf "../publish.tar" *
+    tar -czf "../publish.tar" -- *
     echo "Compress .tar to .tar.gz .."
     gzip -k "../publish.tar"
     echo "Compress .tar to .tar.xz .."
     xz -e9 --threads=0 -f "../publish.tar"
 
     echo "Finished publishing."
-    cd ..
-  cd ..
+  )
+)
