17740c48c3e54bcb18c93d720724016a24c46ae2
Nico Linder <nico@nicoviii.de>
2018-12-27 12:53:28 +0100

    add progressbar and installing text

--- Andromeda.AvaloniaApp/Helper/DownloadStatus.cs
+++ Andromeda.AvaloniaApp/Helper/DownloadStatus.cs
@@ -1,14 +1,30 @@
+using ReactiveUI;
+
 using static GogApi.DotNet.FSharp.GalaxyApi;
 
 namespace Andromeda.AvaloniaApp.Helpers
 {
-    public class DownloadStatus
+    public class DownloadStatus : ReactiveObject
     {
+        private float downloaded;
+        private bool installing;
+
         public string GameTitle { get; private set; }
         public string FilePath { get; private set; }
         public float FileSize { get; private set; }
-        public float Downloaded { get; private set; }
-        public bool Installing { get; private set; }
+
+        public float Downloaded {
+            get => this.downloaded;
+            private set {
+                this.RaiseAndSetIfChanged(ref this.downloaded, value);
+            }
+        }
+        public bool Installing {
+            get => installing;
+            private set {
+                this.RaiseAndSetIfChanged(ref this.installing, value);
+            }
+        }
 
         public DownloadStatus(string gameTitle, string path, float fileSize)
         {
