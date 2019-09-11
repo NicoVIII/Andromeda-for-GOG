d4fc3d96bd58d22621e16b307b62d3e11292b9ab
Nico Linder <nl@fp.de>
2019-09-11 13:48:47 +0200

    remove remnants

--- build-scripts/build-console.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-PROJECT_PATH="src/Andromeda/ConsoleApp"
-dotnet restore -s "https://www.myget.org/F/avalonia-ci/api/v2" -s "https://api.nuget.org/v3/index.json" "$PROJECT_PATH" && dotnet build --no-restore "$PROJECT_PATH"
--- build-scripts/build.cmd
+++
@@ -1,2 +0,0 @@
-SET project_path="src/Andromeda/AvaloniaApp/"
-dotnet restore -s "https://www.myget.org/F/avalonia-ci/api/v2" -s "https://api.nuget.org/v3/index.json" %project_path% && dotnet build --no-restore %project_path%
