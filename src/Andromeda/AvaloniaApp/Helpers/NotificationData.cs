e099eb9d63335674206d9af5b0d0edfdcd8166be
Nico Linder <nl@fp.de>
2019-09-11 13:43:02 +0200

    remove old projects

--- src/Andromeda/AvaloniaApp/Helpers/NotificationData.cs
+++
@@ -1,9 +0,0 @@
-namespace Andromeda.AvaloniaApp.Helpers {
-    public class NotificationData {
-        public string Message { get; set; }
-
-        public NotificationData(string message) {
-            this.Message = message;
-        }
-    }
-}
--- src/Andromeda/ConsoleApp/README.txt
+++
@@ -1,2 +0,0 @@
-This is an early version of an console UI for Andromeda before I started to develop a graphical UI.
-This is not actively maintained although I will try to at least make it work if core code changes.
