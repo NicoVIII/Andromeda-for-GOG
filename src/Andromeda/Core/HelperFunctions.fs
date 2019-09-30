8a89335b1119f7b2e70e9ef6b0acb482c5d2f559
Nico Linder <nico@nicoviii.net>
2019-09-30 21:05:20 +0200

    add some Result helpers

--- src/Andromeda/Core/HelperFunctions.fs
+++ src/Andromeda/Core/HelperFunctions.fs
@@ -60,3 +60,20 @@ module HelperFunctions =
         with
         | _ ->
             ""
+    module Result =
+        let map f xResult =
+            match xResult with
+            | Ok x -> Ok (f x)
+            | Error err -> Error err
+
+        let bind f xResult =
+            match xResult with
+            | Ok x -> f x
+            | Error err -> Error err
+
+    type ResultBuilder() =
+        member this.Return x = Ok x
+        member this.Zero() = Ok ()
+        member this.Bind(xResult,f) = Result.bind f xResult
+
+    let result = ResultBuilder()
