24f8c5f1510e441f76fbbeb8a70b6adf4b5a2447
NicoVIII <nico@nicoviii.net>
2020-09-18 16:44:33 +0000

    Try out net5 with Andromeda

--- src/Andromeda/Core/Types/InstalledGame.fs
+++ src/Andromeda/Core/Types/InstalledGame.fs
@@ -2,7 +2,8 @@ namespace Andromeda.Core.FSharp.DomainTypes
 
 open Myriad.Plugins
 
-[<Generator.Lenses("Andromeda.Core.FSharp.Lenses.Lens")>]
+open Andromeda.Core.FSharp.Lenses
+
 type InstalledGame =
     { id: GogApi.DotNet.FSharp.DomainTypes.ProductId
       name: string
@@ -19,3 +20,30 @@ module InstalledGame =
           version = version
           updateable = false
           icon = None }
+
+module InstalledGameLenses =
+    let id =
+        Lens
+            ((fun (x: InstalledGame) -> x.id),
+             (fun (x: InstalledGame) (value: GogApi.DotNet.FSharp.DomainTypes.ProductId) -> { x with id = value }))
+
+    let name =
+        Lens((fun (x: InstalledGame) -> x.name), (fun (x: InstalledGame) (value: string) -> { x with name = value }))
+
+    let path =
+        Lens((fun (x: InstalledGame) -> x.path), (fun (x: InstalledGame) (value: string) -> { x with path = value }))
+
+    let version =
+        Lens
+            ((fun (x: InstalledGame) -> x.version),
+             (fun (x: InstalledGame) (value: string) -> { x with version = value }))
+
+    let updateable =
+        Lens
+            ((fun (x: InstalledGame) -> x.updateable),
+             (fun (x: InstalledGame) (value: bool) -> { x with updateable = value }))
+
+    let icon =
+        Lens
+            ((fun (x: InstalledGame) -> x.icon),
+             (fun (x: InstalledGame) (value: string option) -> { x with icon = value }))
