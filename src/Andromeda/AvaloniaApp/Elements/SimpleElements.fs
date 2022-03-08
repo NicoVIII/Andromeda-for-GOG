d6d4cf645e8133f339500e093022a4f8b5d20dd0
NicoVIII <nico@nicoviii.net>
2022-03-08 12:07:18 +0000

    Move simple elements together in one file

--- src/Andromeda/AvaloniaApp/Elements/SimpleElements.fs
+++ src/Andromeda/AvaloniaApp/Elements/SimpleElements.fs
@@ -4,6 +4,12 @@ open Avalonia.Controls
 open Avalonia.FuncUI.DSL
 open Avalonia.FuncUI.Types
 
+module SimpleDockPanel =
+    let inline create children =
+        DockPanel.create [
+            DockPanel.children children
+        ]
+
 module SimpleScrollViewer =
     let inline create (content: IView) =
         ScrollViewer.create [
